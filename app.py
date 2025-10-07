8f024b8c7851bf5c634fb457d272ae504d40aee9
Alfez09 <alfez6000@gmail.com>
2025-10-07 14:00:40 +0530

    Initial commit with Streamlit Gemini chatbot

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+import os
+import google.generativeai as genai
 
-from dotenv import load_dotenv 
-load_dotenv() 
-import streamlit as st 
-import os 
-import google.generativeai as genai 
+print("GOOGLE_API_KEY:", os.getenv("GOOGLE_API_KEY"))  # Debug line
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash") 
-# model = genai.GenerativeModel("gemini-pro") 
-# model = genai.GenerativeModel("gemini-1.5-pro") 
+model = genai.GenerativeModel("gemini-2.0-flash")
+
 
 def my_output(query):
-    response = model.generate_content(query) 
-    return response.text 
+    response = model.generate_content(query)
+    return response.text
 
-#### UI Development using streamlit 
 
-st.set_page_config(page_title="MY_bot")
-st.header("MY_bot") 
-input = st.text_input("Input " , key = "input")  
-submit = st.button("Ask your query") 
+## UI Development using streamlit
 
-if submit :
-    response = my_output(input) 
-    st.subheader("The Response is=")
+st.set_page_config(page_title="Allie_bot")
+st.header("Allie_bot")
+input = st.text_input("Input " , key = "input")
+submit = st.button("Ask Your Query")
+
+if submit:
+    response = my_output(input)
+    st.subheader("The response is :")
     st.write(response)
